b16edffc4b66b156c5d6b489e3f9872b4f3cb2a6
Cadzen505 <cadzen707@gmail.com>
2025-11-02 21:48:38 -0700

    testing comment function

--- db/helpers.py
+++ db/helpers.py
@@ -1,4 +1,6 @@
 import tinydb
+# IMPORTANT: Import Query to ensure the environment for TinyDB is fully set up
+from tinydb import Query 
 
 def load_db():
     return tinydb.TinyDB('db.json', sort_keys=True, indent=4, separators=(',', ': '))
